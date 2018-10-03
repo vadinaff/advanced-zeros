a74dbd501674cdb06a4de1d4c03b560b396199e4
vadinaff <43517406+vadinaff@users.noreply.github.com>
2018-10-03 11:16:47 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
-}
+  var number, base;
+var zerosCount=0;
+var k=0;
+    while (number >= base) {
+     number =  parseInt(number / base);
+     k = k + number;
+     
+    }
+    zerosCount=k;
+  return zerosCount;
+  }
