26cd0166bb3c452b38176628b0a8584c94c56e2d
vadinaff <43517406+vadinaff@users.noreply.github.com>
2018-10-04 00:06:52 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,31 @@
 module.exports = function getZerosCount(number, base) {
+  var j = 1;
+  var i = 2;
+  var a = new Array();
+  var det;
+  do
+  {
+   if (base % i == 0) // "x" - Число, которое нам требуется разложить. Должно задаваться заранее.
+   {
+    a[j] = i;
+    j++;
+    base = base / i;
+   }
+   else
+   {
+    i++;
+   }
+  }
+  while (i < base);
+  a[j] = i; // В массиве "a" содержатся вычисленные множители.
+
+
   var number, base;
 var zerosCount=0;
 var k=0;
     while (number >= base) {
      number =  parseInt(number / base);
      k = k + number;
-     
     }
     zerosCount=k;
   return zerosCount;
